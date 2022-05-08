705e7211a6a72d12300b4eca19cc4cf94c9ff6cc
Prakarshprasar <prakprasar@gmail.com>
2022-05-09 01:45:20 +0530

    slider added

--- scripts/index.js
+++ scripts/index.js
@@ -84,3 +84,37 @@ document.getElementById("pr43").addEventListener("click",gotokid);
 document.getElementById("pr44").addEventListener("click",gotokid);
 
 
+
+
+
+let goright=()=>{
+    document.getElementById("pr6img1").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/w_swim/2022may_0426_w_swim_img2.jpg')"
+    document.getElementById("like1").innerText="579 Likes"
+
+    document.getElementById("pr6img2").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/flyout/2022may_0506_flyouts_m_img0.jpg')"
+    document.getElementById("like2").innerText="784 Likes"
+
+    document.getElementById("pr6img3").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/flyout/2022may_0426_flyouts_cc_img2.jpg')"
+    document.getElementById("like3").innerText="1718 Likes"
+
+    document.getElementById("pr6img4").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/flyout/2022may_0506_flyouts_stories_img0.jpg')"
+    document.getElementById("like4").innerText="987 Likes"
+}
+document.getElementById("sign2").addEventListener("click", goright)
+
+
+let goleft=()=>{
+    document.getElementById("pr6img1").style.backgroundImage="url('https://www.jcrew.com/s7-img-facade/BA400_OR5362_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540')"
+    document.getElementById("like1").innerText="416 Likes"
+
+    document.getElementById("pr6img2").style.backgroundImage="url('https://www.jcrew.com/s7-img-facade/BI759_OR5362_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540')"
+    document.getElementById("like2").innerText="793 Likes"
+
+    document.getElementById("pr6img3").style.backgroundImage="url('https://www.jcrew.com/s7-img-facade/BI409_YD1226_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540')"
+    document.getElementById("like3").innerText="1412 Likes"
+
+    document.getElementById("pr6img4").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/flyout/2022may_0426_flyouts_cc_img0.jpg')"
+    document.getElementById("like4").innerText="1629 Likes"
+}
+document.getElementById("sign1").addEventListener("click", goleft)
+
